f152a8ba6bf69d1db6d9928958a791b383669adf
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2026-04-24 00:08:36 -0300

    feat: add kafka infrastructure - topics, producer, consumer and events

---
+++ src/main/java/com/deploymanager/deploy_manager/domain/kafka/AccessRequestEventConsumer.java
@@ -0,0 +1,21 @@
+package com.deploymanager.deploy_manager.domain.kafka;
+
+import com.deploymanager.deploy_manager.domain.kafka.events.AccessRequestEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AccessRequestEventConsumer {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @KafkaListener(topics = "access-requests", groupId = "deploy-manager-group")
+    public void consume (AccessRequestEvent event){
+        simpMessagingTemplate.convertAndSend("/topic/access-requests", event);
+    }
+
+
+}
---
+++ src/main/java/com/deploymanager/deploy_manager/domain/kafka/KafkaProducerService.java
@@ -0,0 +1,20 @@
+package com.deploymanager.deploy_manager.domain.kafka;
+
+import com.deploymanager.deploy_manager.domain.kafka.events.AccessRequestEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducerService {
+
+    private final KafkaTemplate <String, Object> kafkaTemplate;
+
+    public void publishAccessRequestEvent(AccessRequestEvent event){
+        kafkaTemplate.send("access-requests", event);
+    }
+
+
+}
